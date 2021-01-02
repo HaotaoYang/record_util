c25bafc01a21e5a9f5ca33481d2392e75a4ef133
Holtom <520023290@qq.com>
2021-01-03 00:57:02 +0800

    fix error

--- src/record_util_executer.erl
+++ src/record_util_executer.erl
@@ -13,6 +13,20 @@
 -define(DEFAULT_DEST_DIR, "src/").              %% 默认生成文件的路径，如果没有配置，则生成到该路径
 -define(DEFAULT_MODULE_NAME, "record_helper").  %% 默认模块名，如果没有配置，则生成到该模块中
 
+-define(CHECK_VALUES(Values),
+    case check_values(Values) of
+        true -> skip;
+        _ -> exit({error, "Invalid args"})
+    end
+    ).
+
+-define(CHECK_VALUE(Value),
+    case check_value(Value) of
+        true -> skip;
+        _ -> exit({error, "Invalid args"})
+    end
+    ).
+
 %% ===================================================================
 %% Public API
 %% ===================================================================
@@ -33,7 +47,7 @@ exec(ProviderHook, State) ->
     ok.
 
 -spec pre_compile(rebar_app_info:t(), rebar_state:t()) -> ok.
-pre_compile(AppInfo, State) ->
+pre_compile(AppInfo, _State) ->
     % %% 获取项目目录
     % AppDir = rebar_app_info:dir(AppInfo),
     % rebar_api:info("AppDir:~p", [AppDir]),
@@ -48,8 +62,11 @@ pre_compile(AppInfo, State) ->
     Opts = rebar_app_info:opts(AppInfo),
     {ok, RecordUtilOpts} = dict:find(record_util_opts, Opts),
     HrlDirs = proplists:get_value(hrl_dirs, RecordUtilOpts, ?DEFAULT_HRL_DIRS),
+    ?CHECK_VALUES(HrlDirs),
     DestDir = proplists:get_value(dest_dir, RecordUtilOpts, ?DEFAULT_DEST_DIR),
+    ?CHECK_VALUE(DestDir),
     ModuleName = proplists:get_value(module_name, RecordUtilOpts, ?DEFAULT_MODULE_NAME),
+    ?CHECK_VALUE(ModuleName),
     RecordInfos = get_record_infos(HrlDirs),
     AllRecordInfos = get_record_fields(RecordInfos),
     gen_file(DestDir, ModuleName, AllRecordInfos).
@@ -59,24 +76,31 @@ pre_clean(_AppInfo, _State) -> ok.
     
 -spec post_compile(rebar_app_info:t(), rebar_state:t()) -> ok.
 post_compile(_AppInfo, _State) ->
-    rebar_api:info("===> post_compile, but nothing to do...", []),
+    rebar_api:info("record_util post_compile, but nothing to do...", []),
     ok.
 
 -spec post_clean(rebar_app_info:t(), rebar_state:t()) -> ok.
 post_clean(_AppInfo, _State) ->
-    rebar_api:info("===> post_clean, but nothing to do...", []),
+    rebar_api:info("record_util post_clean, but nothing to do...", []),
     ok.
 
 %% ===================================================================
 %% Private API
 %% ===================================================================
+%% 检测配置参数合法性
+check_values(Values) ->
+    lists:all(fun(Value) -> check_value(Value) end, Values).
+
+check_value(Value) when is_atom(Value) orelse is_list(Value) -> true;
+check_value(_) -> false.
+
 %% 获取指定目录内所有文件的record信息
 get_record_infos(HrlDirs) ->
     RecordInfos = lists:flatten([
         begin
             case file:list_dir(Dir) of
                 {ok, Files} ->
-                    [get_file_record_infos(Dir ++ File) || File <- Files, filelib:is_file(Dir ++ File)];
+                    [get_file_record_infos(filename:join([Dir, File])) || File <- Files, filelib:is_file(filename:join([Dir, File]))];
                 _ ->
                     []
             end
@@ -86,9 +110,7 @@ get_record_infos(HrlDirs) ->
         fun(RecordInfo, TempInfos) ->
             {RecordName, _} = RecordInfo,
             case lists:keyfind(RecordName, 1, TempInfos) of
-                {RecordName, _} ->
-                    rebar_api:warn("record redefined, record name:~p", [RecordName]),
-                    TempInfos;
+                {RecordName, _} -> TempInfos;
                 _ -> [RecordInfo | TempInfos]
             end
         end,
@@ -124,10 +146,14 @@ get_record_fields(RecordInfos) ->
     ]).
 
 %% 过滤信息，只留下record字段
-filter_record_field([record_field, _, {_, _, Field}]) -> Field;
+filter_record_field([record_field, _, {_, _, Field} | _]) -> Field;
 filter_record_field([Info | RetInfos]) ->
-    case Info of
-        {record_field, _, {_, _, Field}} -> Field;
+    case is_tuple(Info) of
+        true ->
+            case tuple_to_list(Info) of
+                [record_field, _, {_, _, Field} | _] -> Field;
+                _ -> filter_record_field(RetInfos)
+            end;
         _ -> filter_record_field(RetInfos)
     end.
 
@@ -149,4 +175,3 @@ gen_file(DestDir, ModuleName, AllRecordInfos) ->
     ] ++
     "fields_info(_Other) -> exit({error, \"Invalid Record Name\"}).\n",
     ok = file:write_file(filename:join([DestDir, ModuleName]) ++ ".erl", list_to_binary(Data)).
-
